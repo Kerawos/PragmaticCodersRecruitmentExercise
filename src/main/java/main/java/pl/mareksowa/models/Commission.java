c82ef9f163210e005aaf7f362f99756439d3cf2c
Kerawos <Maresowa@gmail.com>
2017-11-27 15:54:04 +0100

    @update commission model: get deals from system

--- src/main/java/main/java/pl/mareksowa/models/Commission.java
+++ src/main/java/main/java/pl/mareksowa/models/Commission.java
@@ -1,11 +1,38 @@
 package main.java.pl.mareksowa.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Commission {
 
-    public String calculateCommissions(String dateFrom, String dateTo, int agentId){
 
-        //todo implement
+
+    public String calculateCommissions(String dateFrom, String dateTo, int agentId){
+        List<DealsInTheSystem> dealsInTheSystems = new ArrayList<DealsInTheSystem>();
+        dealsInTheSystems = getDealsInTheSystem(agentId);
+        leftOnlyDealsBetweenGivenDate(dealsInTheSystems, dateFrom, dateTo);
+
+        
         return null;
+    }
+
+    public List<DealsInTheSystem> leftOnlyDealsBetweenGivenDate(List<DealsInTheSystem> currentDeals, String dateFrom, String dateTo){
+        //left only deals between given date
+        for (DealsInTheSystem deal : currentDeals) {
+            if (parseDate(deal.getDealDate())<parseDate(dateFrom) && parseDate(deal.getDealDate())>=parseDate(dateTo)){
+                currentDeals.remove(deal);
+            }
+        }
+        return currentDeals;
+    }
+
+    public List<DealsInTheSystem> getDealsInTheSystem( int agentId){
+        // todo implements
+        return null;
+    }
+
+    public int parseDate(String date){
+        return 0;
     }
 
 
